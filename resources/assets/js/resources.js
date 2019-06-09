fbba29b2bce71cb336050656a8b9c89186cd49ec
Rafa <rafag.1858@hotmail.es>
2019-06-09 20:03:00 +0200

    Fix en un recurso que tenía puesto un / al final y eso causaba un error 301 en producción.

--- resources/assets/js/resources.js
+++ resources/assets/js/resources.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 
 export default {
-  user: Vue.resource('user/{/userId}'),
+  user: Vue.resource('user{/userId}'),
   folder: Vue.resource('folder{/entryId}'),
   source: Vue.resource('source{/entryId}'),
   entry:  Vue.resource('entry{/entryId}'),
