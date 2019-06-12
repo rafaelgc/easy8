9bba1219174a2473112c0b872f6e1635dbda0fcb
Rafa <rafag.1858@hotmail.es>
2019-06-12 22:04:26 +0200

    Añadida meta description para mejorar seo.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="Simulador de un procesador didáctico de 8 bits.">
 
         <title>Easy8</title>
 
