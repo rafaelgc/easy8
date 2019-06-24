3cf7ecc055f31ab5ac98b9e643ce4aedb72e59ba
Rafa <rafag.1858@hotmail.es>
2019-06-24 02:15:20 +0200

    Implementando función de copia. Todavía no es correcta porque, si hay carpetas, puede entrar en bucles infinitos.

--- app/Services/FilesystemService.php
+++ app/Services/FilesystemService.php
@@ -18,4 +18,36 @@ class FilesystemService {
 
         return $query->exists();
     }
+
+    public function copy(Entry $entry, $targetId) {
+        $pending = [[$entry, $targetId]];
+
+        while (!empty($pending)) {
+            list($entry, $targetId) = array_pop($pending);
+
+            // Copiar la entrada.
+            $newEntry = new Entry();
+            $newEntry->parent_id = $targetId;
+            $newEntry->owner_id = $entry->owner_id;
+            $newEntry->name = $entry->name;
+            $newEntry->updated_at = $entry->updated_at;
+            $newEntry->created_at = $entry->created_at;
+            $newEntry->save();
+
+            if ($entry->source) {
+                // Copiar también el código asociado, si lo tiene.
+                $newEntry->source()->create([
+                    'content' => $entry->source->content
+                ]);
+            }
+            else {
+                // Se trata de una carpeta, hay que preocuparse de
+                // copiar también todo el contenido de esta.
+                $children = $entry->children();
+                foreach ($children as &$child) {
+                    array_push($pending, [$child, $newEntry->id]);
+                }
+            }
+        }
+    }
 }
